b4f5a1fcc0cbcf79263e01250fd4ec1660d61d11
Waleed Gudah <aquataze@yahoo.com>
2024-03-23 03:30:53 -0500

    Update gmailServiceAccountConfig with latest modifications

--- src/utils/gmailServiceAccountConfig.ts
+++ src/utils/gmailServiceAccountConfig.ts
@@ -4,19 +4,19 @@ class GmailServiceAccountConfig {
     private _serviceAccount: IGmailServiceAccount | null = null;
     private _serviceAccountPath: string | null = null;
 
-    get SERVICE_ACCOUNT(): IGmailServiceAccount | null {
+    get_SERVICE_ACCOUNT(): IGmailServiceAccount | null {
         return this._serviceAccount;
     }
 
-    set SERVICE_ACCOUNT(account: IGmailServiceAccount | null) {
+    set_SERVICE_ACCOUNT(account: IGmailServiceAccount | null) {
         this._serviceAccount = account;
     }
 
-    get SERVICE_ACCOUNT_PATH(): string | null {
+    get_SERVICE_ACCOUNT_PATH(): string | null {
         return this._serviceAccountPath;
     }
 
-    set SERVICE_ACCOUNT_PATH(path: string | null) {
+    set_SERVICE_ACCOUNT_PATH(path: string | null) {
         this._serviceAccountPath = path;
     }
 }
