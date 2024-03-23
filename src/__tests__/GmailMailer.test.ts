200d6a61c00626e269a284843c99f53b3629c6f1
Waleed Gudah <aquataze@yahoo.com>
2024-03-23 04:59:53 -0500

    Update package dependencies and adjust tests for GmailMailer module

--- src/__tests__/GmailMailer.test.ts
+++ src/__tests__/GmailMailer.test.ts
@@ -1,45 +1,47 @@
 import { GmailMailer } from '../GmailMailer';
 import { ISendEmailParams } from '../types'; // Ensure the path is correct
 
+// Define mockGmailClient with the mocked send function
+const mockGmailClient = {
+  users: {
+    messages: {
+      send: jest.fn().mockResolvedValue({
+        status: 200,
+        data: { id: '123', labelIds: ['SENT'] },
+      }),
+    },
+  },
+};
+
 describe('GmailMailer', () => {
-  let mockGmailClient: any; // Adjust the typing as needed
-
-  beforeEach(() => {
-    // Setup the mockGmailClient with the necessary mocked functions
-    mockGmailClient = {
-      users: {
-        messages: {
-          send: jest.fn().mockResolvedValue({ status: 200, data: { id: '123', labelIds: ['SENT'] } }),
-        },
-      },
-    };
-  });
-
   describe('initializeClient', () => {
     it('should initialize the Gmail client successfully', async () => {
+      const gmailMailer = new GmailMailer(mockGmailClient); // Use the mock client
       const mockServiceAccount = {
         client_email: 'test@example.com',
-        private_key: '-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n',
+        private_key: `-----BEGIN PRIVATE KEY-----\\n...\\n-----END PRIVATE KEY-----\\n`,
       };
-      const gmailMailer = new GmailMailer(mockGmailClient); // Inject the mock
 
-      await expect(gmailMailer.initializeClient({
-        gmailServiceAccount: mockServiceAccount,
-        gmailSenderEmail: 'sender@example.com',
-      })).resolves.toEqual({
+      await expect(
+        gmailMailer.initializeClient({
+          gmailServiceAccount: mockServiceAccount,
+          gmailSenderEmail: 'sender@example.com',
+        })
+      ).resolves.toEqual({
         status: true,
         gmailClient: expect.anything(),
-        message: "Successfully initialized the Gmail API client."
+        message: 'Successfully initialized the Gmail API client.',
       });
     });
   });
 
   describe('sendEmailWrapper', () => {
     it('should send an email successfully', async () => {
-      const gmailMailer = new GmailMailer(mockGmailClient); // Inject the mock
-      
+      // Initialize GmailMailer with the mocked Gmail client
+      const gmailMailer = new GmailMailer(mockGmailClient);
+
       const emailParams: ISendEmailParams = {
-        senderEmail: 'sender@example.com', // Assuming this email is configured if needed
+        // Assuming senderEmail is optional and can use a default or previously set value
         recipientEmail: 'recipient@example.com',
         subject: 'Test Subject',
         message: 'This is a test email.',
