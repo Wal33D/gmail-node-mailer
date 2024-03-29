8dbca4e77dbdce837946ae56db4c02b7f04f7f85
Waleed Gudah <aquataze@yahoo.com>
2024-03-29 03:47:40 -0500

    Fix/Polynomial regular expression used on uncontrolled data

--- src/utils/isValidEmail.ts
+++ src/utils/isValidEmail.ts
@@ -1,25 +1,26 @@
 /**
  * Checks if the provided string is a valid email address.
  * 
- * This function uses a regular expression to validate the email format. It checks for a general
- * pattern that includes characters before the "@" symbol, followed by characters after it,
- * and finally a domain suffix. This is a basic validation and might not cover all edge cases
- * of email address validation according to the RFC specifications.
+ * This function uses a regular expression to validate the email format. It checks for a pattern that
+ * includes one or more characters before the "@" symbol, followed by characters after the "@" symbol,
+ * and finally a domain suffix with two or more characters, allowing for subdomains. This pattern avoids
+ * unnecessary backtracking and complexity, making it more performant and less vulnerable to ReDoS attacks.
  *
  * @param {{ email: string }} params An object containing the email address to validate.
  * @returns {{ status: boolean, result: boolean | null }} Object containing the status of the operation
  * and the result of the email validation.
  */
-import validator from 'validator';
-
-export function isValidEmail({ email }: { email: string }): { status: boolean, result: boolean } {
+export function isValidEmail({ email }: { email: string }): { status: boolean, result: boolean | null } {
     let status = false;
-    let result: boolean = false;
+    let result: boolean | null = null;
     try {
-        result = validator.isEmail(email);
+        // Efficient regex pattern for basic email validation
+        const emailRegex = /^[^@\s]+@([^@\s]+\.)+[^@\s]{2,}$/;
+        result = emailRegex.test(email);
         status = true; // Operation succeeded
     } catch (error: any) {
         console.error("Error validating email:", error.message);
+        result = false; // Explicitly setting result to false in case of error
     }
     return { status, result };
 }
